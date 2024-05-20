dbd79cdacd0f4dd07d485e21c5326df7ab1af9cf
pukpuk555 <jester2540@gmail.com>
2024-05-20 14:29:22 +0700

    CSS LGTM

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 function changeText() {
   let heading = document.getElementById("heading");
+  let button = document.querySelector(".onButton");
+  let button2 = document.querySelector("#lisBtn");
+  //text
   heading.textContent = "Hello JavaScript InnerHTML Onclick";
+  heading.style.color = "pink";
+  heading.style.backgroundColor = "black";
+  heading.style.fontWeight = "bold";
+  heading.style.fontSize = "3rem";
+  //button
+  button.style.fontSize = "1rem";
+  button.style.paddingLeft = "2rem";
+  button.style.paddingRight = "2rem";
+  button.style.backgroundColor = "grey";
+  //button2
+  button2.style = "none";
 }
 
 document.querySelector("#lisBtn").addEventListener("click", function () {
   let heading = document.getElementById("heading");
+  let button = document.querySelector(".onButton");
+  let button2 = document.querySelector("#lisBtn");
+  //text
   heading.textContent = "Hello JavaScript InnerHTML EventListener";
+  heading.style.color = "pink";
+  heading.style.backgroundColor = "black";
+  heading.style.fontWeight = "bold";
+  heading.style.fontSize = "3rem";
+  //button
+  button.style = "none";
+  //button2
+  button2.style.fontSize = "1rem";
+  button2.style.paddingLeft = "2rem";
+  button2.style.paddingRight = "2rem";
+  button2.style.backgroundColor = "grey";
 });
